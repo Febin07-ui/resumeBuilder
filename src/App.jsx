a1b70ee2acee561500322e9b9ea2fc29bdbeb386
Febin07-ui <febinkulangara23@gmail.com>
2025-10-23 12:19:40 +0800

    resumebuilder created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes } from "react-router-dom"
+import './App.css'
+import LandingPage from './pages/LandingPage'
+import ResumeGenerator from './pages/ResumeGenerator'
+import UserForm from './pages/UserForm'
+import History from './pages/History'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
